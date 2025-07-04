c7d11f10373cc2aaa6ae40c8b8f5624ccf66596e
sh78ba <sh78ba@gmail.com>
2025-07-04 18:17:11 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.send('👋 Hello from Smart Portfolio Backend!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
